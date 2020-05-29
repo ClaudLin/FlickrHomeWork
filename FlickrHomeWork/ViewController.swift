9e54600f7f7613303d762782a0054e29e1de4bae
Claud <claud25315488@gmail.com>
2020-05-29 15:04:24 +0800

    Initial Commit

---
+++ FlickrHomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrHomeWork
+//
+//  Created by 1900347_Claud on 2020/5/29.
+//  Copyright © 2020 1900347_Claud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
